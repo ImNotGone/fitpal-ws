6018d8c2b843a4d5b1e22bada10ea80da26ac493
Juan Cruz Bafico <86856551+jBafico@users.noreply.github.com>
2022-09-28 12:15:22 -0300

    Started creating NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav>
+    <!--
+    <div class="d-inline-flex flex-xl-column pa-5 mt-5 secondary text--info">
+     <h1>FitPal</h1>
+      <div><v-btn flat class="secondary">Click</v-btn></div>
+      <div><v-btn flat>Click</v-btn></div>
+      <div><v-btn flat>Click</v-btn></div>
+      <div><v-btn flat>Click</v-btn></div>
+    </div>
+    -->
+    <v-navigation-drawer app class="black">
+      <v-toolbar-title>FitPal</v-toolbar-title>
+     <div align="center">
+       <v-btn plain>
+         <v-icon left>
+         </v-icon>
+         Home
+       </v-btn>
+     </div>
+      <div align="center">
+        <v-btn plain>
+          <v-icon left>
+          </v-icon>
+          Routines
+        </v-btn>
+      </div>
+      <div align="center">
+        <v-btn plain>
+          <v-icon left>
+          </v-icon>
+          Trainers
+        </v-btn>
+      </div>
+      <div align="center">
+        <v-btn plain>
+          <v-icon left>
+          </v-icon>
+          Settings
+        </v-btn>
+      </div>
+    </v-navigation-drawer>
+    <v-toolbar vertical flat app>
+      <v-toolbar-title>FitPal</v-toolbar-title>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,24 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify,{
+    iconfont: 'md',
+    theme:{//You can use it with class="class"
+        primary: '#FF8754', //Primary 500 Orange
+        secondary: '#353535', //Primary 0 Black
+        accent: '#848484', //Secondary 400 Grey this is for background
+        info: '#FFFFFF', //Secondary 100 White this is for text
+    }
+});
 
 export default new Vuetify({
+    iconfont: 'md',
+    theme:{//You can use it with class="class"
+        primary: '#FF8754', //Primary 500 Orange
+        secondary: '#353535', //Primary 0 Black
+        accent: '#848484', //Secondary 400 Grey this is for background
+        info: '#FFFFFF', //Secondary 100 White this is for text
+    }
 });
+
+
