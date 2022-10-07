54d014ef2c88e2ccdaae232a6528ca27f55bf5f3
Cuinardium <sballerini@itba.edu.ar>
2022-10-07 00:46:23 -0300

    ADD: created detailed exercise component

---
+++ src/components/Detailed/DetailedExercise.vue
@@ -0,0 +1,105 @@
+<!-- Detailed view of an exercise -->
+<!-- First row is a video of the exercise -->
+<!-- Second row is the exercise name -->
+<!-- Third row is the exercise tags -->
+<!-- Fourth row is the amount of times the exercise appears in other routines -->
+<!-- Fifth row is the exercise description -->
+<!-- Sixth row is the muscles used by the exercise and a button to add the exercise to a routine -->
+
+
+<template>
+  <v-card class="grey darken-3 white--text">
+    <!-- Video of the exercise -->
+    <video
+      class="mx-auto"
+      width="100%"
+      height="auto"
+      controls
+      :src="video"
+    ></video>
+
+    <!-- Exercise name -->
+    <v-card-title primary-title>
+      <div>
+        <h3 class="headline mb-0">{{ exerciseName }}</h3>
+      </div>
+    </v-card-title>
+
+    <!-- Exercise tags -->
+    <v-card-actions>
+      <v-chip-group column>
+        <v-chip v-for="tag in tags" :key="tag" color="primary" text-color="white" small>
+          {{ tag }}
+        </v-chip>
+      </v-chip-group>
+    </v-card-actions>
+
+    <!-- Amount of times the exercise appears in other routines -->
+    <!-- Icon amount Routines -->
+    <v-card-text>
+      <v-row>
+        <v-icon class="pr-3 ml-3" color="white">mdi-account-multiple-outline</v-icon>
+        <span class="white--text">In {{ amountRoutines }} Routines</span>
+      </v-row>
+    </v-card-text>
+
+    <!-- Exercise description -->
+    <v-card-title primary-title>
+      <div>
+        <h3 class="headline mb-0">Description</h3>
+      </div>
+    </v-card-title>
+    <v-card-text class="white--text">
+      <p>{{ exerciseDescription }}</p>
+    </v-card-text>
+
+    <!-- A list of muscles used by the exercise and a button to add the exercise to a routine at the bottom right -->
+    <v-card-text class="white--text">
+      <v-row>
+        <v-col cols="8">
+          <h4 class="headline pb-3">Essence of exercise</h4>
+          <!-- Vertical Column of muscles used by the exercise -->
+          <v-col cols="8">
+            <v-row class="pb-3" v-for="muscle in muscles" :key="muscle">
+              <v-icon class="pr-3" color="primary">mdi-check</v-icon>
+              <span class="white--text">{{ muscle }}</span>
+            </v-row>
+          </v-col>
+
+        </v-col>
+
+
+        <v-col cols="4" align-self="end">
+
+          <!-- TODO cambiar el css feo -->
+          <v-btn
+            color="primary"
+            depressed
+            class="white--text pa-5"
+            style="float: right"
+          >
+            Add to routine
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+
+</template>
+
+<script>
+export default {
+  name: "DetailedExercise",
+  props: {
+    video: String,
+    exerciseName: String,
+    exerciseDescription: String,
+    amountRoutines: Number,
+    tags: Array,
+    muscles: Array,
+  },
+}
+</script>
+
+<style scoped>
+</style>
