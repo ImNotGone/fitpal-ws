69bad1690c8b643887ba57044e28979e4cfcb901
Cuinardium <sballerini@itba.edu.ar>
2022-10-13 21:28:30 -0300

    FIX: Signup works with code & added error handling

--- src/views/FPSignUp.vue
+++ src/views/FPSignUp.vue
@@ -1,106 +1,119 @@
 <template>
   <div class="fill-width fill-height accent">
     <TopToolbar/>
-  <v-container>
-  <v-card dark class="secondary">
-    <router-link :to="pathBack">
-      <v-icon large class="white--text">mdi-chevron-left</v-icon>
-    </router-link>
+    <v-container>
+      <v-card dark class="secondary">
+        <router-link :to="pathBack">
+          <v-icon large class="white--text">mdi-chevron-left</v-icon>
+        </router-link>
 
-    <v-img class="logo" src="@/assets/fitpal-horizontallogo.svg"/>
+        <v-img class="logo" src="@/assets/fitpal-horizontallogo.svg"/>
 
-    <v-card-title>
-      Sign Up
-    </v-card-title>
+        <v-card-title>
+          Sign Up
+        </v-card-title>
 
-  <v-form
-      ref="form"
-      v-model="form"
-      class="pa-4 pt-6"
-  >
-    <v-text-field
-        v-model="firstName"
-        filled
-        :rules="[rules.required]"
-        color="primary"
-        label="First Name"
-        type="firstName"
-        @input="$v.name.$touch()"
-        @blur="$v.name.$touch()"
-    ></v-text-field>
-    <v-text-field
-        v-model="lastName"
-        filled
-        :rules="[rules.required]"
-        color="primary"
-        label="Last Name"
-        type="lastName"
-        @input="$v.name.$touch()"
-        @blur="$v.name.$touch()"
-    ></v-text-field>
-    <v-text-field
-        v-model="email"
-        :rules="[rules.email, rules.required]"
-        filled
-        color="primary"
-        label="Email address"
-        type="email"
-        @input="$v.name.$touch()"
-        @blur="$v.name.$touch()"
-    ></v-text-field>
-    <v-text-field
-        v-model="password"
-        :rules="[rules.password, rules.length(8), rules.required]"
-        filled
-        color="primary"
-        label="Password"
-        style="min-height: 96px"
-        type="password"
-        @input="$v.name.$touch()"
-        @blur="$v.name.$touch()"
-    ></v-text-field>
-    <v-text-field
-        v-model="password2"
-        :rules="[rules.password, rules.length(8), rules.required]"
-        filled
-        color="primary"
-        label="Confirm Password"
-        style="min-height: 96px"
-        type="password"
-        @input="$v.name.$touch()"
-        @blur="$v.name.$touch()"
-    ></v-text-field>
-    <v-select
-        v-model="selectedChoice"
-        :items="trainer"
-        :rules="[rules.required]"
-        label="Are you a Trainer?"
-    ></v-select>
-    <v-btn
-        class="primary mr-4"
-        :loading="loading"
-        :disabled="loading"
-        @click="submit"
-    >
-      Sign Up
-    </v-btn>
-    <v-dialog persistent v-model="showCard" max-width="600px">
-      <v-card dark class="secondary pa-5">
-        <v-card-title class="justify-center">Verification</v-card-title>
-        <v-card-text class="justify-center">An email has been sent to {{ email }}, please click the link in it or enter the code below to verify your account</v-card-text>
-        <v-text-field dark v-model="vCode" type="vCode" label="Verification Code"></v-text-field>
-        <v-row class="pa-5">
-          <v-btn class="primary pa-5" :loading="loading"
-                 :disabled="loading" @click="verify">Verify</v-btn>
-          <v-spacer/>
-          <v-btn :disabled="logIn" class="primary pa-5" to="/login">Log In</v-btn>
-        </v-row>
+        <v-form
+            ref="form"
+            class="pa-4 pt-6"
+        >
+          <v-text-field
+              v-model="firstName"
+              filled
+              :rules="[rules.required]"
+              color="primary"
+              label="First Name"
+              type="firstName"
+          ></v-text-field>
+          <v-text-field
+              v-model="lastName"
+              filled
+              :rules="[rules.required]"
+              color="primary"
+              label="Last Name"
+              type="lastName"
+          ></v-text-field>
+          <v-text-field
+              v-model="email"
+              :rules="[rules.email, rules.required]"
+              filled
+              color="primary"
+              label="Email address"
+              type="email"
+          ></v-text-field>
+          <v-text-field
+              v-model="password"
+              :rules="[rules.password, rules.length(8, 50), rules.required]"
+              filled
+              color="primary"
+              label="Password"
+              style="min-height: 96px"
+              type="password"
+          ></v-text-field>
+          <v-text-field
+              v-model="password2"
+              :rules="[passwordConfirmationRule, rules.required]"
+              filled
+              color="primary"
+              label="Confirm Password"
+              style="min-height: 96px"
+              type="password"
+          ></v-text-field>
+          <v-select
+              v-model="selectedChoice"
+              :items="trainer"
+              :rules="[rules.required]"
+              label="Are you a Trainer?"
+          ></v-select>
+          <v-btn
+              class="primary mr-4 mt-3"
+              :loading="loading"
+              :disabled="loading"
+              @click="submit"
+          >
+            Sign Up
+          </v-btn>
 
+          <!-- Signup Error Message -->
+          <p v-if="signupError" class="error--text align-center mb-n3 mt-1">
+            {{ signupErrorText }}
+          </p>
+
+
+          <v-dialog persistent v-model="showCard" max-width="600px">
+            <v-card v-if="!verified" dark class="secondary pa-5">
+              <v-card-title class="justify-center">Verification</v-card-title>
+              <v-card-text class="justify-center">An email has been sent to {{ email }}, please click the link in it or
+                enter the code below to verify your account
+              </v-card-text>
+              <v-text-field dark v-model="vCode" type="vCode" label="Verification Code"></v-text-field>
+              <!-- Show error message if verification code is incorrect -->
+              <v-card-text v-if="verificationError" class="justify-center ml-n6 error--text"> {{
+                  verificationErrorText
+                }}
+              </v-card-text>
+              <v-card-text v-if="verified" class="justify-center ml-n6"> Your account has been verified!</v-card-text>
+
+              <v-btn class="primary pa-5" :loading="loading"
+                     :disabled="loading" @click="verify">Verify
+              </v-btn>
+            </v-card>
+
+            <!-- Verification success card -->
+            <v-card v-else dark class="secondary pa-5">
+              <v-card-title>
+                <v-row>
+                  <v-icon large color="primary" class="pr-3">mdi-check-circle</v-icon>
+                  Your account has been verified!
+                  <v-spacer/>
+                  <v-btn class="primary pa-5" to="/login">Log In</v-btn>
+                </v-row>
+              </v-card-title>
+            </v-card>
+          </v-dialog>
+        </v-form>
       </v-card>
-    </v-dialog>
-  </v-form>
-  </v-card>
-  </v-container>
+    </v-container>
     <NoLoginFooter/>
   </div>
 </template>
@@ -114,11 +127,26 @@ export default {
   name: "FPSignUp",
   components: {NoLoginFooter, TopToolbar},
   data: () => ({
-    logIn: true,
-    showCard: false,
+    // For verification dialog card
+    // Disables the login button until the user has verified their email
     verified: false,
+
+    signupError: false,
+    signupErrorText: '',
+
+    verificationError: false,
+    verificationErrorText: '',
+
+    // Shows the verification dialog card
+    showCard: false,
+    // Verification code
     vCode: '',
     pathBack: '/landing-page',
+
+    // For button loading
+    loading: false,
+
+    // Form data
     firstName: '',
     lastName: '',
     email: '',
@@ -129,55 +157,90 @@ export default {
       'No',
     ],
     selectedChoice: '',
+
+    // Form validation
     rules: {
-      email: v => !!(v || '').match(/@/) || 'Please enter a valid email',
-      length: len => v => (v || '').length >= len || `Invalid character length, required ${len}`,
+      email: v => !!(v || '').match(/.+@.+\..+/) || 'Please enter a valid email',
+      length: (minLen, maxLen) => v => ((v || '').length >= minLen && (v || '').length <= maxLen) || `Invalid character length, required more than ${minLen} and less than ${maxLen}`,
       password: v => !!(v || '').match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/) ||
           'Password must contain an upper case letter, a lower case letter and a numeric character',
       required: v => !!v || 'This field is required',
     },
-    loading: false,
   }),
   methods: {
-    async submit () {
-      this.loading = true;
-      const creds = new RegistrationCredentials(this.firstName, this.lastName, this.email, this.password);
-      try {
-        console.log(await UserApi.signup(creds))
-        this.showCard=true;
-        // TODO: MANDARLO A LA PAGINA DE VERIFICACION O ALGO
-        // POR EJEMPLO -> verify -> login
-        // router.replace({path: '/verify?email=${this.email}&code='''})
-        //  o sino mostrar un mensaje diciendo que vaya al mail a verificarse desde ahi
-      } catch (error) {
-        console.log(error)
-        // TODO: MOSTRAR EL ERROR EN PANTALLA
+    async submit() {
+
+      // Check if form is valid
+      if (!this.$refs.form.validate()) {
+        return;
       }
+
+      this.loading = true;
+
+      // Create registration object
+      const creds = new RegistrationCredentials(this.firstName, this.lastName, this.email, this.password);
+
+      try {
+        // Register user
+        await UserApi.signup(creds);
+
+        // Show verification dialog card
+        this.showCard = true;
+
+      } catch (error) {
+
+        this.signupError = true;
+
+        if (error.code === 1) {
+          this.signupErrorText = 'Invalid Data'; // This should never happen
+        } else if (error.code === 2) {
+          this.signupErrorText = 'Email already in use';
+        } else {
+          this.signupErrorText = 'An error occurred, please try again later';
+        }
+      }
+
       this.loading = false;
     },
+
     async verify() {
       this.loading = true;
+
+      this.verificationError = false;
+
+      // Verify the code
       try {
+        // Verification object
         const accountVerify = new AccountVerify(this.email, this.vCode);
+
         await UserApi.verify_email(accountVerify);
-        this.logIn= true;
-      } catch(error) {
-        alert(error);
-        this.failed = false;
+      } catch (error) {
+        this.verificationError = true;
+
+        if (error.code === 8) {
+          this.verificationErrorText = error.details[0];
+        } else {
+          this.verificationErrorText = 'An error occurred, please try again later';
+        }
+
+        this.loading = false;
+        return;
       }
       this.verified = true;
       this.loading = false;
     },
   },
+  computed: {
+    passwordConfirmationRule() {
+      return () => (this.password === this.password2) || 'Password must match'
+    }
+  }
 }
 </script>
 
 <style scoped>
-.logo{
+.logo {
   width: 50%;
   margin: 0 auto;
-}
-.form{
-
 }
 </style>
